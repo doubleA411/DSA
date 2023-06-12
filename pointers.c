0a793bedd549fc47534059ea5b5c7a33a443265b
Aakash <aakashsuresh62@gmail.com>
2023-06-12 12:56:08 +0530

    pointers

--- pointers.c
+++ pointers.c
@@ -1,13 +1,62 @@
 #include <stdio.h>
 
-int main () {
-    int arr[] = {40,10,60,7};
-    int* ptr = arr;
-    int temp = *ptr;
-    for(int i = 1; i<4;i++){
-        if(temp < *(ptr+i)){
-            temp = *(ptr+i);
-        }
-    } 
-    printf("%d",temp);
-}
+// Write a program to find the largest element in an array using pointers.
+
+// int main () {
+//     int arr[] = {40,10,60,7};
+//     int size = sizeof(arr)/sizeof(int);
+//     int* ptr = arr;
+//     int temp = *ptr;
+//     for(int i = 1; i<size;i++){
+//         if(temp < *(ptr+i)){
+//             temp = *(ptr+i);
+//         }
+//     } 
+//     printf("%d",temp);
+// }
+
+
+// Write a program to calculate the average of all elements in a floating-point array using pointers.
+
+// int main(){
+
+//     int arr[] = {10, 20, 30, 5};
+//     int size = sizeof(arr)/sizeof(int);
+//     int* ptr = arr;
+//     float sum = 0.0;
+//     for(int i = 0; i<size; i++){
+//         sum = sum + * (ptr + i);
+//     }
+//     printf("%f",sum/size);
+// }
+
+// Write a program to reverse the elements of an array using pointers.
+
+
+// int main() {
+
+//     int arr[] = {10, 20, 30, 5};
+//     int size = sizeof(arr)/sizeof(int);
+//     int* ptr = arr;
+
+//     for ( int i = size - 1; i >= 0; i--){
+//         printf("%d ",*(ptr + i));
+//     }
+// }
+
+// Write a program to find the length of a string using pointers.
+
+// int main() {
+
+//     char string[10] = "hello";
+//     char* ptr = string;
+//     int len = 0;
+
+//     while(*ptr != '\0'){
+//         len++;
+//         ptr++;
+//     }
+//     printf("%d",len);
+
+// }
+
